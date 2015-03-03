07159705327eb7fdf690063b5ac794d05394da45
ujali <ujali@knoldus.com>
2015-03-03 13:29:29 +0530

    initial set up for project simplePage

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:/home/ujali/Projects/simplePage/simplePage/conf/routes
+// @HASH:4f41fe2b6f59f0d1d4aec1fd7f4615d1d3133677
+// @DATE:Tue Mar 03 13:17:44 IST 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
